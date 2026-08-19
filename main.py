69a2c22a8fd66873c5c06ba9b529ec4755ff2243
Rakesh <mrakeshrout34@gmail.comgit config --global user.email mrakeshrout34@gmail.com>
2026-08-19 12:56:39 +0530

    new functions added

---
+++ main.py
@@ -0,0 +1,64 @@
+import json
+
+INPUT_FILE = "page_map (2).json"
+OUTPUT_FILE = "sorted_date_by_date.json"
+
+def get_dos_date(page):
+
+    dates = page.get("metadata", {}).get("candidate_dos_dates", [])
+    # print(dates)
+
+    if not dates:
+        return (0, "")
+
+    date_value = dates[0].get("value")
+
+    if not date_value:
+        return (0, "")
+    # print(1,date_value)
+    return (1,date_value)
+
+def get_provider(page):
+
+    provider = page.get("metadata", {}).get("provider")
+
+
+    if isinstance(provider, dict):
+        return provider.get("value") or ""
+    
+    return ""
+
+
+def sort_by_date(pages):
+    pages.sort(key=get_dos_date)
+    return pages
+
+def sort_by_provider(pages):
+    pages.sort(key=get_provider)
+    return pages
+
+
+
+
+# data["pages"].sort(key=get_provider)  #Sort pages by provider name
+# print(data["pages"])
+# data["pages"].sort(key=get_dos_date)   # Sort pages by DOS date
+
+if __name__ == "__main__":
+
+    with open(INPUT_FILE, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    sort_by_date(data["pages"])
+
+    with open(OUTPUT_FILE, "w", encoding="utf-8") as file:
+        json.dump(data, file, indent=2, ensure_ascii=False)
+
+
+
+# def all_provider_name():
+#     for i, page in enumerate(data["pages"], start=1):
+#         dos_value = get_provider(page)
+#         print(f"Page {i}: {dos_value}")
+# all_provider_name()
+
